434436a64900bc2e591e8ec150410f87c6bcdf11
Danny Barrett <jamesdbarrett1992@gmail.com>
2024-04-11 20:13:37 -0700

    added phone number to nav bar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+// import { useEffect } from 'react'
 import BackToTop from './BackToTop'
 import Footer from './Footer'
 import GoogleAnalytics from './GoogleAnalytics'
@@ -15,6 +16,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
+  // useEffect(() => {
+  //   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  //   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  //   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  //   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  //   })(window,document,'script','dataLayer','GTM-K6GQPDW2');
+  // })
+
   return (
     <html lang="en" className={oxygen.className}>
       <head>
@@ -23,7 +32,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;500;600&display=swap" rel="stylesheet" />
       </head>
-      <body className='w-screen max-w-[100vw] overflow-x-clip'>
+      <body className='w-screen max-w-[100vw] overflow-x-clip relative'>
         <BackToTop className="absolute bottom-0 right-0" />
         <Nav />
         {children}
