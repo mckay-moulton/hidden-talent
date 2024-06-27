9a8fbbca8839cb6acf065416170dd661d564396e
mckay-moulton <mckaymoulton1@gmail.com>
2024-06-27 07:30:24 -0600

    Removed some text in Job Listings

--- app/listings/Description.jsx
+++ app/listings/Description.jsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Description() {
   return (
     <motion.p
-      initial={{ opacity: 0, y: 200 }} 
-      whileInView={{ opacity: 1, y: 0 }} 
-      viewport={{ once: true }} 
-      transition={{ duration: 0.35 }} 
+      initial={{ opacity: 0, y: 200 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.35 }}
     >
-      We are eager to help you find your dream job. Explore our open positions below and click the button to learn more.
+      {/* We are eager to help you find your dream job. Explore our open positions below and click the button to learn more. */}
     </motion.p>
-  )
+  );
 }
