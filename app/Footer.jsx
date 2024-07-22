38647b14682737dde6bc04679cc1f87d063a3d9d
mckay-moulton <mckaymoulton1@gmail.com>
2024-07-22 17:48:37 -0600

    added banner with logos

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -4,43 +4,44 @@ export default function Footer() {
   return (
     <footer className="bg-hidden-black flex flex-col items-center content-center justify-center justify-items-center px-4 py-8 gap-6 text-white text-center relative z-[99]">
       <Link href="/" className="flex items-center justify-center">
-          <img
-            src="/hidden-talent-logo-full-width-dark.png"
-            width="243"
-            height="34"
-            alt="Home"
-            className="w-4/5 lg:w-1/3"
-          />
+        <img
+          src="/hidden-talent-logo-full-width-dark.png"
+          width="243"
+          height="34"
+          alt="Home"
+          className="w-4/5 lg:w-1/3"
+        />
       </Link>
-      <p>We are proudly a family owned, minority-owned, faith-based business.</p>
+      <p>
+        We are proudly a family-owned, minority-owned, faith-based business.
+      </p>
       <div className="flex justify-center gap-4">
-        <Link href="https://www.facebook.com/profile.php?id=61551467866738" target="_blank">
-          <img 
-            src="/icon_facebook.svg"
-            width="36"
-            height="36"
-            alt="Facebook"
-          />
+        <Link
+          href="https://www.facebook.com/profile.php?id=61551467866738"
+          target="_blank"
+        >
+          <img src="/icon_facebook.svg" width="36" height="36" alt="Facebook" />
         </Link>
-        <Link href="https://www.instagram.com/hire_hidden_talent" target="_blank">
-          <img 
+        <Link
+          href="https://www.instagram.com/hire_hidden_talent"
+          target="_blank"
+        >
+          <img
             src="/icon_instagram.svg"
             width="36"
             height="36"
             alt="Instagram"
           />
         </Link>
-        <Link href="https://www.linkedin.com/company/hire-hidden-talent/about/" target="_blank">
-          <img 
-            src="/icon_linkedin.svg"
-            width="36"
-            height="36"
-            alt="Linkedin"
-          />
+        <Link
+          href="https://www.linkedin.com/company/hire-hidden-talent/about/"
+          target="_blank"
+        >
+          <img src="/icon_linkedin.svg" width="36" height="36" alt="Linkedin" />
         </Link>
       </div>
-      <p>&copy; 2024 Hidden Talent</p>
+      <p>&copy; 2024 Hidden Talent | All Rights Reserved</p>
       {/* <p>Created by <Link href="https://www.varial.dev" className="text-hidden-teal-dark" target="_blank">Varial Web Development</Link></p> */}
     </footer>
-  )
+  );
 }
--- app/Intro.jsx
+++ app/Intro.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 
@@ -9,149 +9,211 @@ import { useEffect } from "react";
 
 export default function IntroSection() {
   useEffect(() => {
-    gsap.registerPlugin(_ScrollTrigger)
+    gsap.registerPlugin(_ScrollTrigger);
 
-    gsap.from('#bumper-left', {
-      scrollTrigger: '#bumper-left',
+    gsap.from("#bumper-left", {
+      scrollTrigger: "#bumper-left",
       left: -200,
       opacity: 0,
       duration: 0.35,
-    })
-    gsap.to('#bumper-left', {
-      scrollTrigger: '#bumper-left',
+    });
+    gsap.to("#bumper-left", {
+      scrollTrigger: "#bumper-left",
       left: 0,
       opacity: 1,
-    })
+    });
 
-    gsap.from('#bumper-right', {
-      scrollTrigger: '#bumper-right',
+    gsap.from("#bumper-right", {
+      scrollTrigger: "#bumper-right",
       left: -200,
       opacity: 0,
       duration: 0.35,
-    })
-    gsap.to('#bumper-right', {
-      scrollTrigger: '#bumper-right',
+    });
+    gsap.to("#bumper-right", {
+      scrollTrigger: "#bumper-right",
       left: 0,
       opacity: 1,
-    })
+    });
 
-    gsap.from('#intro-h1', {
-      scrollTrigger: '#intro-h1',
+    gsap.from("#intro-h1", {
+      scrollTrigger: "#intro-h1",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#intro-h1', {
-      scrollTrigger: '#intro-h1',
+    });
+    gsap.to("#intro-h1", {
+      scrollTrigger: "#intro-h1",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#changing-careers', {
-      scrollTrigger: '#changing-careers',
+    gsap.from("#changing-careers", {
+      scrollTrigger: "#changing-careers",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#changing-careers', {
-      scrollTrigger: '#changing-careers',
+    });
+    gsap.to("#changing-careers", {
+      scrollTrigger: "#changing-careers",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#hiring-your-team', {
-      scrollTrigger: '#hiring-your-team',
+    gsap.from("#hiring-your-team", {
+      scrollTrigger: "#hiring-your-team",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#hiring-your-team', {
-      scrollTrigger: '#hiring-your-team',
+    });
+    gsap.to("#hiring-your-team", {
+      scrollTrigger: "#hiring-your-team",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#stats', {
-      scrollTrigger: '#stats',
+    gsap.from("#stats", {
+      scrollTrigger: "#stats",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#stats', {
-      scrollTrigger: '#stats',
+    });
+    gsap.to("#stats", {
+      scrollTrigger: "#stats",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#start-your-search', {
-      scrollTrigger: '#start-your-search',
+    gsap.from("#start-your-search", {
+      scrollTrigger: "#start-your-search",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#start-your-search', {
-      scrollTrigger: '#start-your-search',
+    });
+    gsap.to("#start-your-search", {
+      scrollTrigger: "#start-your-search",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#talent', {
-      scrollTrigger: '#talent',
+    gsap.from("#talent", {
+      scrollTrigger: "#talent",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#talent', {
-      scrollTrigger: '#talent',
+    });
+    gsap.to("#talent", {
+      scrollTrigger: "#talent",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#talent-h2', {
-      scrollTrigger: '#talent-h2',
+    gsap.from("#talent-h2", {
+      scrollTrigger: "#talent-h2",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#talent-h2', {
-      scrollTrigger: '#talent-h2',
+    });
+    gsap.to("#talent-h2", {
+      scrollTrigger: "#talent-h2",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#areas-h2', {
-      scrollTrigger: '#areas-h2',
+    gsap.from("#areas-h2", {
+      scrollTrigger: "#areas-h2",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#areas-h2', {
-      scrollTrigger: '#areas-h2',
+    });
+    gsap.to("#areas-h2", {
+      scrollTrigger: "#areas-h2",
       opacity: 1,
       y: 0,
-    })
+    });
 
-    gsap.from('#areas-served', {
-      scrollTrigger: '#areas-served',
+    gsap.from("#areas-served", {
+      scrollTrigger: "#areas-served",
       opacity: 0,
       y: 200,
       duration: 0.35,
-    })
-    gsap.to('#areas-served', {
-      scrollTrigger: '#areas-served',
+    });
+    gsap.to("#areas-served", {
+      scrollTrigger: "#areas-served",
       opacity: 1,
       y: 0,
-    })
-
-  }, [])
+    });
+  }, []);
 
   return (
     <section className="relative grid grid-cols-[auto_1fr_auto] justify-center justify-items-center items-center justify-content-center mx-auto w-fit lg:w-full">
-      <img src="/bumper-left.svg" id="bumper-left" width="24" className="hidden lg:block lg:w-10 mt-32 opacity-0" />
-      <section className='flex flex-col gap-8 lg:gap-16 px-4 py-12 lg:py-16 items-center relative'>
-        <h1 id="intro-h1" className='text-[29px] md:text-[42px] text-center'>
-          Delivering <span className='text-hidden-teal'>Great People</span> to <span className='text-hidden-teal md:block lg:inline'>Great Companies</span>
+      <img
+        src="/bumper-left.svg"
+        id="bumper-left"
+        width="24"
+        className="hidden lg:block lg:w-10 mt-32 opacity-0"
+      />
+      <section className="flex flex-col gap-8 lg:gap-16 px-4 py-12 lg:py-16 items-center relative">
+        <h1 id="intro-h1" className="text-[29px] md:text-[42px] text-center">
+          Delivering <span className="text-hidden-teal">Great People</span> to{" "}
+          <span className="text-hidden-teal md:block lg:inline">
+            Great Companies
+          </span>
         </h1>
+
+        {/* <!-- New Banner Section --> */}
+        <div className="flex items-center justify-center bg-[#07A6A2] py-8 w-full">
+          <div className="flex justify-between w-full max-w-4xl px-8">
+            <div className="flex flex-col items-center">
+              <span className="mb-2 text-2xl">
+                <b>ECDVC</b>
+              </span>
+              <a
+                href="https://www.ecdvc.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://hidden-talent-images.s3.amazonaws.com/logos/ECDVC_black+(2).png"
+                  alt="ECDVC Logo"
+                  className="h-16"
+                />
+              </a>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="mb-3 text-2xl">
+                <b>Stack Construction</b>
+              </span>
+              <a
+                href="https://stackconstruction.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://hidden-talent-images.s3.amazonaws.com/logos/Stack_New.png"
+                  alt="Stack Logo"
+                  className="h-16"
+                />
+              </a>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="mb-3 text-2xl">
+                <b>KIT</b>
+              </span>
+              <a
+                href="https://www.kitwest.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://hidden-talent-images.s3.amazonaws.com/logos/KIT_new.png"
+                  alt="KIT Logo"
+                  className="h-16"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
         {/* <div className='flex flex-col gap-8 lg:gap-24 lg:flex-row max-w-[1280px]'>
           <div 
             id="changing-careers" 
@@ -172,34 +234,70 @@ export default function IntroSection() {
             <img src="/businesswoman.svg" alt="" width="107" className='relative justify-center self-center md:absolute md:right-2' />
           </div>
         </div> */}
-        <div 
-          id="stats" 
-          className='border-2 border-hidden-teal rounded-[50px] p-6 md:p-12 md:mt-12 w-fit max-w-[1024px] mx-auto flex flex-col gap-8'
+        <div
+          id="stats"
+          className="border-2 border-hidden-teal rounded-[50px] p-6 md:p-12 md:mt-12 w-fit max-w-[1024px] mx-auto flex flex-col gap-8"
         >
-          <h2 className='text-xl text-center'>We bring employees and employers together.</h2>
-          <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-24 items-center content-center text-center'>
-            <div className='grid  gap-4 items-center justify-center justify-items-center lg:h-full'>
+          <h2 className="text-xl text-center">
+            We bring employees and employers together.
+          </h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-24 items-center content-center text-center">
+            <div className="grid  gap-4 items-center justify-center justify-items-center lg:h-full">
               <img src="handshake.svg" alt="" width="115" />
-              <p className="self-end"><span className='block text-hidden-teal text-3xl font-bold'>100+</span> <span className='block'>jobs</span> placed</p>
+              <p className="self-end">
+                <span className="block text-hidden-teal text-3xl font-bold">
+                  100+
+                </span>{" "}
+                <span className="block">jobs</span> placed
+              </p>
             </div>
-            <div className='grid gap-4 items-center lg:h-full justify-items-center'>
+            <div className="grid gap-4 items-center lg:h-full justify-items-center">
               <img src="office-buildings.svg" alt="" width="115" />
-              <p className="self-end"><span className='block text-hidden-teal text-3xl font-bold'>5</span> <span className='block'>industries</span> served</p>
+              <p className="self-end">
+                <span className="block text-hidden-teal text-3xl font-bold">
+                  5
+                </span>{" "}
+                <span className="block">industries</span> served
+              </p>
             </div>
-            <div className='grid gap-4 items-center lg:h-full justify-items-center'>
+            <div className="grid gap-4 items-center lg:h-full justify-items-center">
               <img src="networking.svg" alt="" width="115" />
-              <p className="self-end"><span className='block text-hidden-teal text-3xl font-bold'>50</span> <span className='block'>professionals</span> helped</p>
+              <p className="self-end">
+                <span className="block text-hidden-teal text-3xl font-bold">
+                  50
+                </span>{" "}
+                <span className="block">professionals</span> helped
+              </p>
             </div>
-            <div className='grid gap-4 items-center lg:h-full justify-items-center'>
-              <img src="experience.svg" alt="" height="115" className="relative left-2.5" />
-              <p><span className='block text-hidden-teal text-3xl font-bold'>10</span> <span className='block'>years</span> experience</p>
+            <div className="grid gap-4 items-center lg:h-full justify-items-center">
+              <img
+                src="experience.svg"
+                alt=""
+                height="115"
+                className="relative left-2.5"
+              />
+              <p>
+                <span className="block text-hidden-teal text-3xl font-bold">
+                  10
+                </span>{" "}
+                <span className="block">years</span> experience
+              </p>
             </div>
           </div>
         </div>
         <div className="grid lg:grid-cols-2 gap-8">
           <div className="grid gap-6 md:px-16">
-            <h2 id="talent-h2" className="text-[29px] md:text-[42px] text-center"><span className="text-hidden-teal">Talent</span> We Have Discovered</h2>
-            <div id="talent" className="border border-hidden-teal rounded-[20px] p-5 md:p-6 grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-6 max-w-[620px] mx-auto">
+            <h2
+              id="talent-h2"
+              className="text-[29px] md:text-[42px] text-center"
+            >
+              <span className="text-hidden-teal">Talent</span> We Have
+              Discovered
+            </h2>
+            <div
+              id="talent"
+              className="border border-hidden-teal rounded-[20px] p-5 md:p-6 grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-6 max-w-[620px] mx-auto"
+            >
               <div className="grid gap-2.5 justify-center justify-items-center">
                 <img src="/accountant.svg" alt="" width="25" />
                 <span className="text-center">Accountant</span>
@@ -263,20 +361,35 @@ export default function IntroSection() {
             </div>
           </div>
           <div id="areas" className="grid gap-6 md:px-16">
-            <h2 id="areas-h2" className="text-[29px] md:text-[42px] text-center">Where We <span className="text-hidden-teal">Serve</span></h2>
-            <img id="areas-served" src="https://media.hirehiddentalent.com/images/homepage-images/areas-served" alt="Alaska, Oregon, Idaho, Montana, Wyoming, Nevada, Utah, Arizona, New Mexico" />
+            <h2
+              id="areas-h2"
+              className="text-[29px] md:text-[42px] text-center"
+            >
+              Where We <span className="text-hidden-teal">Serve</span>
+            </h2>
+            <img
+              id="areas-served"
+              src="https://media.hirehiddentalent.com/images/homepage-images/areas-served"
+              alt="Alaska, Oregon, Idaho, Montana, Wyoming, Nevada, Utah, Arizona, New Mexico"
+            />
           </div>
         </div>
         <p id="start-your-search" className="mt-8">
-          <Link 
-            href="/contact" 
-            className='bg-gradient-to-b from-[#11B0AC] to-[#03D8D4] hover:from-[#ffffff] hover:to-[#ffffff] hover:text-hidden-teal border border-[#fff] hover:border-hidden-teal text-center rounded-full py-5 uppercase font-semibold text-lg text-white px-12'
+          <Link
+            href="/contact"
+            className="bg-gradient-to-b from-[#11B0AC] to-[#03D8D4] hover:from-[#ffffff] hover:to-[#ffffff] hover:text-hidden-teal border border-[#fff] hover:border-hidden-teal text-center rounded-full py-5 uppercase font-semibold text-lg text-white px-12"
           >
             Start your search
           </Link>
         </p>
       </section>
-      <img src="/bumper-left.svg" id="bumper-right" width="24" className="hidden lg:block lg:w-10 mt-32 opacity-0" style={{ transform: 'scaleX(-1)'}} />
+      <img
+        src="/bumper-left.svg"
+        id="bumper-right"
+        width="24"
+        className="hidden lg:block lg:w-10 mt-32 opacity-0"
+        style={{ transform: "scaleX(-1)" }}
+      />
     </section>
-  )
+  );
 }
